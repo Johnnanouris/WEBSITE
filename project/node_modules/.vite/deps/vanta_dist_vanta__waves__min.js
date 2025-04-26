3510b17b3241b9875be36e4b781fa367b0f75782
Johnnanouris <166568219+Johnnanouris@users.noreply.github.com>
2025-04-27 02:27:21 +0300

    Delete project directory

--- project/node_modules/.vite/deps/vanta_dist_vanta__waves__min.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_vanta_waves_min
-} from "./chunk-SJ77OPRW.js";
-import "./chunk-G3PMV62Z.js";
-export default require_vanta_waves_min();
--- project/src/vanta.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'vanta/dist/vanta.waves.min.js' {
-    function WAVES(options: any): any;
-    export default WAVES;
-  }
